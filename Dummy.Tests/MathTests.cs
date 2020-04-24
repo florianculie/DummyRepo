609e56685400b9143c9452b8f89ecf898dbcccb6
Déromba Serge Bissari <dbissari@gmail.com>
2020-04-24 16:03:38 +0200

    Initial Commit

---
+++ Dummy.Tests/MathTests.cs
@@ -0,0 +1,35 @@
+using System;
+using Xunit;
+
+namespace Dummy.Tests
+{
+    public class MathTests
+    {
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(1, 2)]
+        [InlineData(3, 1)]
+        [InlineData(1, 4)]
+        [InlineData(5, 1)]
+        public void Add_AddsTwoNumbers(int a, int b)
+        {
+            Assert.Equal(a + b, Math.Add(a, b));
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(3, 6)]
+        [InlineData(4, 24)]
+        public void Factorial_ReturnsTheRightAnswers(int n, int result)
+        {
+            Assert.Equal(result, Math.Factorial(n));
+        }
+
+        [Fact]
+        public void Factorial_ThrowsExceptionWhenArgumentOutOfRange()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Math.Factorial(0));
+        }
+    }
+}
---
+++ Dummy/Math.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Dummy
+{
+    public static class Math
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Factorial(int n)
+        {
+            if (n < 1) throw new ArgumentOutOfRangeException(nameof(n));
+
+            if (n == 1)
+                return n;
+            else
+                return n * Factorial(n - 1);
+        }
+    }
+}
